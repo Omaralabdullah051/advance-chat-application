c46d6a953d11f8bd7ba364a999850cb319b0c52a
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-10-25 23:15:24 +0400

    updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,10 +2,10 @@ export default function Footer() {
   return (
     <section className="pt-6">
       <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex justify-between gap-2 border-t text-sm text-slate-400">
-        <p>Copyright 2022</p>
-      </div>
-      <div>
-        <p>omaralabdullah051@gmail.com</p>
+        <div>Copyright 2022</div>
+        <div>
+          <p>omaralabdullah051@gmail.com</p>
+        </div>
       </div>
     </section>
   );
